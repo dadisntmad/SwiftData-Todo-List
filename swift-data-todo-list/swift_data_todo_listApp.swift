d9ed41bf369859a3403871d3076c755add8a2c6e
Ruslan <ruslan.lantsov.m@gmail.com>
2025-10-14 20:51:45 +0300

    Initial Commit

---
+++ swift-data-todo-list/swift_data_todo_listApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_data_todo_listApp.swift
+//  swift-data-todo-list
+//
+//  Created by Ruslan Lantsov on 14.10.2025.
+//
+
+import SwiftUI
+
+@main
+struct swift_data_todo_listApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
